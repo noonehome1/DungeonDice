5ad65e53fa2224adf235936dd332db4508b7de80
Paul Wagstaff <paul@pwfotos.ca>
2025-11-26 07:31:49 -0700

    Initial Commit

---
+++ DungeonDice/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  DungeonDice
+//
+//  Created by Paul Wagstaff on 2025-11-26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ DungeonDice/DungeonDiceApp.swift
@@ -0,0 +1,17 @@
+//
+//  DungeonDiceApp.swift
+//  DungeonDice
+//
+//  Created by Paul Wagstaff on 2025-11-26.
+//
+
+import SwiftUI
+
+@main
+struct DungeonDiceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
